1644faf0ad3cc2563d55a4a156997761abec66c6
Andrea Serafini <andrea.serafini11@studio.unibo.it>
2021-03-22 12:34:57 +0100

    Exercise 1: implemented ComplexImpl as class and case class

--- src/u04lab/code/Complex.scala
+++ src/u04lab/code/Complex.scala
@@ -2,26 +2,43 @@ package u04lab.code
 
 trait Complex {
   def re: Double
+
   def im: Double
+
   def +(c: Complex): Complex // should implement the sum of two complex numbers..
   def *(c: Complex): Complex // should implement the product of two complex numbers
 }
 
 object Complex {
-  def apply(re:Double, im:Double):Complex = ??? // Fill here
+  def apply(re: Double, im: Double): Complex = new ComplexImpl(re, im)
+
+  private case class ComplexImpl(override val re: Double, override val im: Double) extends Complex {
+
+
+    assert(re != null && im != null)
+
+    override def +(c: Complex): Complex = Complex(re+c.re,im+c.im)
+
+    override def *(c: Complex): Complex = Complex(re*c.re - im*c.im,re*c.im + im*c.re)
+  }
+
 }
 
 object TryComplex extends App {
-  val a = Array(Complex(10,20), Complex(1,1), Complex(7,0))
+  val a = Array(Complex(10, 20), Complex(1, 1), Complex(7, 0))
   val c = a(0) + a(1) + a(2)
   println(c, c.re, c.im) // (ComplexImpl(18.0,21.0),18.0,21.0)
   val c2 = a(0) * a(1)
   println(c2, c2.re, c2.im) // (ComplexImpl(-10.0,30.0),-10.0,30.0)
+
+  if(a(1)==Complex(1,1)){
+    println(a(1))
+  }
 }
 
 /** Hints:
-  * - implement Complex with a ComplexImpl class, similar to PersonImpl in slides
-  * - check that equality and toString do not work
-  * - use a case class ComplexImpl instead, creating objects without the 'new' keyword
-  * - check equality and toString now
-  */
+ * - implement Complex with a ComplexImpl class, similar to PersonImpl in slides
+ * - check that equality and toString do not work
+ * - use a case class ComplexImpl instead, creating objects without the 'new' keyword
+ * - check equality and toString now
+ */
